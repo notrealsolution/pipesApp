1bb34b9c75af89ea1e3c204fb46e247e5259df61
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-13 11:00:43 -0600

    Instalando primeflex

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./basics-page.component.css']
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'daniel';
+  public nameUppercase: string = 'ANTONIO';
+  public fullName: string = 'dAnIeL aNtOnIo';
 }
