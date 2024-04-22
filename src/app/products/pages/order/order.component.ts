f4f76341bd4e61656be92e13eb61893702ba567e
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-22 06:49:16 -0600

    Finalizando Pipe personalizados

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -10,6 +10,7 @@ import { Hero, Color } from '../../interfaces/hero.interface';
 export class OrderComponent {
 
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero;
 
   public heroes: Hero[] = [
     {
@@ -43,5 +44,9 @@ export class OrderComponent {
 
     this.isUpperCase = ! this.isUpperCase;
 
+  }
+
+  changeOrder(value: keyof Hero){
+    this.orderBy = value;
   }
 }
